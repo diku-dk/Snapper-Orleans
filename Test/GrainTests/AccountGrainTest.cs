74e95b27d9ae962333cc6dd7b0037dc77ced0326
Vivek Shah <bonii@di.ku.dk>
2019-03-21 13:49:53 +0100

    Added test cases checking transfer transactions for all transaction types

--- Test/GrainTests/AccountGrainTest.cs
+++ Test/GrainTests/AccountGrainTest.cs
@@ -15,7 +15,12 @@ namespace Test.GrainTests
     {
         ClientConfiguration config;
         IClusterClient client;
+        Random rand = new Random();
         readonly uint numOfCoordinators = 10;
+        readonly int maxAccounts = 1000;
+        readonly int maxTransferAmount = 10;
+        readonly int numSequentialTransfers = 10;
+        readonly int numConcurrentTransfers = 1000;
 
         [TestMethod]
         public void test()
@@ -41,83 +46,167 @@ namespace Test.GrainTests
             }            
         }
 
-
-        private async void TestTransfers(int numTransfers=1, bool sequential=true)
+        private List<Tuple<uint, uint, float, bool>> GenerateTransferInformation(int numTuples, Tuple<int, int> fromAccountRange, Tuple<int, int> toAccountRange, Tuple<int, int> transferAmountRange, Tuple<bool, bool> hybridTypeRange)
         {
+            var transferInformation = new List<Tuple<uint, uint, float, bool>>();
+            while (numTuples-- > 0)
+            {
+                var fromAccount = (uint)rand.Next(fromAccountRange.Item1, fromAccountRange.Item2);
+                var toAccount = (uint)rand.Next(toAccountRange.Item1, toAccountRange.Item2);
+                while (fromAccount == toAccount)
+                {
+                    if ((fromAccountRange.Item2 - fromAccountRange.Item1) > (toAccountRange.Item2 - toAccountRange.Item1))
+                    {
+                        fromAccount = (uint)rand.Next(fromAccountRange.Item1, fromAccountRange.Item2);
+                    }
+                    else
+                    {
+                        toAccount = (uint)rand.Next(toAccountRange.Item1, toAccountRange.Item2);
+                    }
+                }
+
+                var transferAmount = (float)rand.Next(transferAmountRange.Item1, transferAmountRange.Item2);
+                bool transactionType = (hybridTypeRange.Item1 == hybridTypeRange.Item2) ? hybridTypeRange.Item1 : Convert.ToBoolean(rand.Next(0, 1));
+                transferInformation.Add(new Tuple<uint, uint, float, bool>(fromAccount, toAccount, transferAmount, transactionType));
+            }
+            return transferInformation;
+        }
+
+
+        private async Task TestTransfers(bool sequential, List<Tuple<uint,uint,float, bool>> transferInformation)
+        {
+            //Read balances prior to transfer
+            var accountBalances = new Dictionary<Guid, float>();
+            foreach (var transferInfoTuple in transferInformation)
+            {
+                var fromId = Helper.convertUInt32ToGuid(transferInfoTuple.Item1);
+                var toId = Helper.convertUInt32ToGuid(transferInfoTuple.Item2);
+                var fromAccount = client.GetGrain<IAccountGrain>(fromId);
+                var toAccount = client.GetGrain<IAccountGrain>(toId);
+
+                Task<FunctionResult> t1 = fromAccount.StartTransaction("GetBalance", new FunctionInput());
+                await t1;
+                Task<FunctionResult> t2 = toAccount.StartTransaction("GetBalance", new FunctionInput());
+                await t2;
+                Assert.IsFalse(t1.Result.hasException() && t2.Result.hasException());
+                accountBalances[fromId] = (float)t1.Result.resultObject;
+                accountBalances[toId] = (float)t2.Result.resultObject;
+            }
+
+            var taskInfo = new List<Tuple<Guid, Guid, float, Task<FunctionResult>>>();
+            var tasks = new List<Task<FunctionResult>>();
+            foreach (var transferInfoTuple in transferInformation)
+            {
+                var fromId = Helper.convertUInt32ToGuid(transferInfoTuple.Item1);
+                var toId = Helper.convertUInt32ToGuid(transferInfoTuple.Item2);
+                var fromAccount = client.GetGrain<IAccountGrain>(fromId);
+                var toAccount = client.GetGrain<IAccountGrain>(toId);
+
+                var args = new TransferInput(transferInfoTuple.Item1, transferInfoTuple.Item2, transferInfoTuple.Item3);
+                var input = new FunctionInput(args);
+                Task<FunctionResult> task;
+                if (transferInfoTuple.Item4)
+                {
+                    //Deterministic transaction
+                    var grainAccessInformation = new Dictionary<Guid, Tuple<String, int>>();
+                    grainAccessInformation.Add(fromId, new Tuple<string, int>("AccountTransfer.Grains.AccountGrain", 1));
+                    grainAccessInformation.Add(toId, new Tuple<string, int>("AccountTransfer.Grains.AccountGrain", 1));
+                    task = fromAccount.StartTransaction(grainAccessInformation, "Transfer", input);                    
+                } else
+                {
+                    //Non-deterministic transaction
+                    task = fromAccount.StartTransaction("Transfer", input);
+                }
+                taskInfo.Add(new Tuple<Guid, Guid, float, Task<FunctionResult>>(fromId, toId, transferInfoTuple.Item3, task));
+                tasks.Add(task);
+                if (sequential)
+                {
+                    await task;
+                }
+            }
+            if(!sequential)
+            {
+                await Task.WhenAll(tasks);
+            }
+            foreach(var aTaskInfo in taskInfo)
+            {
+                if(!aTaskInfo.Item4.Result.hasException())
+                {
+                    accountBalances[aTaskInfo.Item1] -= aTaskInfo.Item3;
+                    accountBalances[aTaskInfo.Item2] += aTaskInfo.Item3;                    
+                }
+            }
             
+            foreach (var transferInfoTuple in transferInformation)
+            {
+                var fromId = Helper.convertUInt32ToGuid(transferInfoTuple.Item1);
+                var toId = Helper.convertUInt32ToGuid(transferInfoTuple.Item2);
+                var fromAccount = client.GetGrain<IAccountGrain>(fromId);
+                var toAccount = client.GetGrain<IAccountGrain>(toId);
+
+                Task<FunctionResult> t1 = fromAccount.StartTransaction("GetBalance", new FunctionInput());
+                await t1;
+                Task<FunctionResult> t2 = toAccount.StartTransaction("GetBalance", new FunctionInput());
+                await t2;
+                Assert.IsFalse(t1.Result.hasException() && t2.Result.hasException());
+                Assert.IsTrue((float)t1.Result.resultObject == accountBalances[fromId]);
+                Assert.IsTrue((float)t2.Result.resultObject == accountBalances[toId]);
+            }
         }
         
         [TestMethod]
         public async Task TestSingleDetTransfer()
         {
-            Console.WriteLine("Event 1");
-            IAccountGrain fromAccount = client.GetGrain<IAccountGrain>(Helper.convertUInt32ToGuid(1));
-            IAccountGrain toAccount = client.GetGrain<IAccountGrain>(Helper.convertUInt32ToGuid(2));
-            IATMGrain atm = client.GetGrain<IATMGrain>(Helper.convertUInt32ToGuid(3));
-            Console.WriteLine("Event 2");
-
-            Guid fromId = fromAccount.GetPrimaryKey();
-            Guid toId = toAccount.GetPrimaryKey();
-            Guid atmId = atm.GetPrimaryKey();
-            Console.WriteLine("Event 3");
-            var grainAccessInformation = new Dictionary<Guid, Tuple<String, int>>();
-            grainAccessInformation.Add(fromId, new Tuple<string, int>("AccountTransfer.Grains.AccountGrain", 1));
-            grainAccessInformation.Add(toId, new Tuple<string, int>("AccountTransfer.Grains.AccountGrain", 1));
-            grainAccessInformation.Add(atmId, new Tuple<string, int>("AccountTransfer.Grains.ATMGrain", 1));
-            Console.WriteLine("Event 4");
-            var args = new TransferInput(1, 2, 10);
-            FunctionInput input = new FunctionInput(args);
-
-            //Deterministic Transactions
-            var t1 = atm.StartTransaction(grainAccessInformation, "Transfer", input);
-            Console.WriteLine("Event 5");
-            var t2 = atm.StartTransaction(grainAccessInformation, "Transfer", input);
-            Console.WriteLine("Event 6");
-            var t3 = atm.StartTransaction(grainAccessInformation, "Transfer", input);
-            Console.WriteLine("Event 7");
-            await Task.WhenAll(t1, t2, t3);
-            Console.WriteLine("Event 7");            
+            var transferInfo = GenerateTransferInformation(1, new Tuple<int, int>(0, maxAccounts/2), new Tuple<int, int>((maxAccounts/2)+1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(true, true));
+            await TestTransfers(true, transferInfo);
         }
 
         [TestMethod]
-        public void TestSequentialDetTransfer()
+        public async Task TestSequentialDetTransfer()
         {
-            throw new AssertFailedException("Unimplemented");
+            var transferInfo = GenerateTransferInformation(numSequentialTransfers, new Tuple<int, int>(0, maxAccounts / 2), new Tuple<int, int>((maxAccounts / 2) + 1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(true, true));
+            await TestTransfers(true, transferInfo);
         }
 
         [TestMethod]
-        public void TestConcurrentDetTransfer()
+        public async Task TestConcurrentDetTransfer()
         {
-            throw new AssertFailedException("Unimplemented");
+            var transferInfo = GenerateTransferInformation(numConcurrentTransfers, new Tuple<int, int>(0, maxAccounts / 2), new Tuple<int, int>((maxAccounts / 2) + 1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(true, true));
+            await TestTransfers(false, transferInfo);
         }
 
         [TestMethod]
-        public void TestSingleNonDetTransfer()
+        public async Task TestSingleNonDetTransfer()
         {
-            throw new AssertFailedException("Unimplemented");
+            var transferInfo = GenerateTransferInformation(1, new Tuple<int, int>(0, maxAccounts / 2), new Tuple<int, int>((maxAccounts / 2) + 1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(false, false));
+            await TestTransfers(true, transferInfo);
         }
 
         [TestMethod]
-        public void TestSequentialNonDetTransfer()
+        public async Task TestSequentialNonDetTransfer()
         {
-            throw new AssertFailedException("Unimplemented");
+            var transferInfo = GenerateTransferInformation(numSequentialTransfers, new Tuple<int, int>(0, maxAccounts / 2), new Tuple<int, int>((maxAccounts / 2) + 1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(false, false));
+            await TestTransfers(true, transferInfo);
         }
         [TestMethod]
-        public void TestConcurrentNonDetTransfer()
+        public async Task TestConcurrentNonDetTransfer()
         {
-            throw new AssertFailedException("Unimplemented");
-        }
-
-        [TestMethod]
-        public void TestHybridSequentialTransfer()
-        {
-            throw new AssertFailedException("Unimplemented");
+            var transferInfo = GenerateTransferInformation(numConcurrentTransfers, new Tuple<int, int>(0, maxAccounts / 2), new Tuple<int, int>((maxAccounts / 2) + 1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(false, false));
+            await TestTransfers(false, transferInfo);            
         }
 
         [TestMethod]
-        public void TestHybridConcurrentTransfer()
+        public async Task TestHybridSequentialTransfer()
         {
-            throw new AssertFailedException("Unimplemented");
+            var transferInfo = GenerateTransferInformation(numSequentialTransfers, new Tuple<int, int>(0, maxAccounts / 2), new Tuple<int, int>((maxAccounts / 2) + 1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(false, true));
+            await TestTransfers(true, transferInfo);
+        }
+
+        [TestMethod]
+        public async Task TestHybridConcurrentTransfer()
+        {
+            var transferInfo = GenerateTransferInformation(numSequentialTransfers, new Tuple<int, int>(0, maxAccounts / 2), new Tuple<int, int>((maxAccounts / 2) + 1, maxAccounts), new Tuple<int, int>(1, maxTransferAmount), new Tuple<bool, bool>(false, true));
+            await TestTransfers(false, transferInfo);
         }
     }
 }
