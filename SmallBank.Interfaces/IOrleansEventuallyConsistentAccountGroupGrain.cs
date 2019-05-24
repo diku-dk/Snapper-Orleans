927f0652efabce89cc0d74c5998656127440ddff
Vivek Shah <bonii@di.ku.dk>
2019-05-24 10:44:08 +0200

    Adding a version of Smallbank to use eventual consistency without any transaction machinery

---
+++ SmallBank.Interfaces/IOrleansEventuallyConsistentAccountGroupGrain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utilities;
+using Orleans.Concurrency;
+using System.Threading.Tasks;
+
+namespace SmallBank.Interfaces
+{    
+    public interface IOrleansEventuallyConsistentAccountGroupGrain : Orleans.IGrainWithIntegerKey, Orleans.IGrainWithGuidKey
+    {
+        [AlwaysInterleave]
+        Task<FunctionResult> StartTransaction(String startFunction, FunctionInput inputs);
+    }
+}
