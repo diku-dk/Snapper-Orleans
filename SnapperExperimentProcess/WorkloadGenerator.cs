f02338827d6d8d678d1873a6ee59ade9a6ece2e2
Yijian Liu <jhs316@ku.dk>
2022-06-01 14:17:22 +0200

    update workload generator

--- SnapperExperimentProcess/WorkloadGenerator.cs
+++ SnapperExperimentProcess/WorkloadGenerator.cs
@@ -170,7 +170,7 @@ namespace SnapperExperimentProcess
             if (Constants.multiSilo == false) return 1;
             
             var sample = numSiloDist.Sample(); // sample = [0, 100)
-            if (sample < 100) return 2;
+            if (sample < workload.distPercent) return 2;
             else return 1;
         }
 
